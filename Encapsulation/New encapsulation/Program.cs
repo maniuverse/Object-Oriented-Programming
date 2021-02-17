ac4328db8515f60652f3c1635659375b2984794e
maniuverse <maniuverse@gmail.com>
2021-02-17 05:57:10 +0530

    Encapusulation

---
+++ Encapsulation/New encapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialization => the process of crating an object for a class
+            Vehicles v1 = new Vehicles();
+            
+                v1.Regno="TN 8567";
+                v1.Liters=5;
+                v1.Wheels=2;
+                Console.WriteLine(v1.getfinalamount(5, 50)); 
+                Console.ReadKey();
+            
+           
+        }
+    }
+}
---
+++ Encapsulation/New encapsulation/Vehicles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_encapsulation
+{
+    class Vehicles
+    {
+        //underscore variables are private variables
+        string _regno;
+        int _wheels;
+        int _liters;
+
+        //properties
+        public string Regno
+        {
+            get { return _regno; }
+            set { _regno = value; }
+        }
+        public int Wheels
+        {
+            get { return _wheels; }
+            set { _wheels = value; }
+        }
+        public int Liters
+        {
+            get { return _liters; }
+            set { _liters = value; }
+        }
+
+
+        //methods
+        private int fillair(int price)
+        {
+            return _wheels * price;
+        }
+        private int  fillfuel(int price)
+        {
+            return _liters*price;
+        }
+        public string getfinalamount(int air, int fuel)
+        {
+            int y=fillair(air)+fillfuel(fuel);
+                return " The total amount to be paid by "+_regno+" is "+y;
+
+        }
+
+    }
+}
